f1fce3c3e7ad801f20443ccbafa9baf904735050
Ammar Alquran <ammaralquran@Ammars-MacBook-Air.local>
2023-04-16 18:15:02 -0700

    adding UI files

---
+++ app/src/main/java/com/example/queuecumber/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.queuecumber
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+    }
+}
