b7e3c28888bb2dc926bf34afc3b8298ba34ac6e1
LofiTurtle <46658877+LofiTurtle@users.noreply.github.com>
2023-04-25 15:57:43 -0700

    Implement native splash screen

--- app/src/main/java/com/example/queuecumber/Homepage.kt
+++ app/src/main/java/com/example/queuecumber/Homepage.kt
@@ -3,18 +3,24 @@ package com.example.queuecumber
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.queuecumber.utils.ApiUtil
 
 
 class Homepage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_homepage)
 
         val welcomeText = findViewById<TextView>(R.id.welcomeText)
-        ApiUtil.userInfoRequest(this) { response ->
-            welcomeText.setText("Welcome, " + response.getString("display_name") + "!")
-        }
+        ApiUtil.userInfoRequest(this, { response ->
+            welcomeText.text = "Welcome, " + response.getString("display_name") + "!"
+        }, { error ->
+            android.util.Log.e("Homepage", error.toString())
+            error.message?.let { android.util.Log.e("Homepage", it) }
+            welcomeText.text = "Couldn't get information"
+        })
     }
 
     override fun onResume() {
