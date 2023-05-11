fdc9f048cab83e446fe57beaa908f916ffeb6dea
AmmarAlq <ammar.alquran@sjsu.edu>
2023-05-10 19:24:50 -0700

    Added history page logic

--- app/src/main/java/com/example/queuecumber/HistoryHomePage.kt
+++ app/src/main/java/com/example/queuecumber/HistoryHomePage.kt
@@ -3,7 +3,11 @@ package com.example.queuecumber
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
 import android.widget.ImageButton
+import android.widget.LinearLayout
+import com.example.queuecumber.utils.ApiUtil
 
 class HistoryHomePage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +18,17 @@ class HistoryHomePage : AppCompatActivity() {
         backButtonH.setOnClickListener {
             val intent = Intent(this, Homepage::class.java)
             startActivity(intent)
+        }
+
+        ApiUtil.historyRequest(this) { response ->
+            val history = response.getJSONArray("history_items")
+            for (i in 0 until history.length()) {
+                val view: LinearLayout =
+                    LayoutInflater.from(this).inflate(R.layout.history_list_element, null) as LinearLayout
+                (view.getChildAt(0) as Button).text = history.getString(i)
+                val historyList = findViewById<LinearLayout>(R.id.history_list_layout)
+                historyList.addView(view)
+            }
         }
     }
 }
