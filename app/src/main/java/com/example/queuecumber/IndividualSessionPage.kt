80c72a531fe00b85b58b2e995f6d18a5b04158e6
AmmarAlq <ammar.alquran@sjsu.edu>
2023-05-14 15:21:16 -0700

    New Listening Session Pages

---
+++ app/src/main/java/com/example/queuecumber/IndividualSessionPage.kt
@@ -0,0 +1,11 @@
+package com.example.queuecumber
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IndividualSessionPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_individual_session_page)
+    }
+}
---
+++ app/src/main/java/com/example/queuecumber/ListeningSessionsPage.kt
@@ -0,0 +1,11 @@
+package com.example.queuecumber
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListeningSessionsPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_listening_sessions_page)
+    }
+}
